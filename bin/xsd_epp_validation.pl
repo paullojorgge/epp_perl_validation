4473a3eb490d388961699b1f768d7e59e43c2490
Paulo Jorge <paullojorgge@gmail.com>
2018-01-23 10:34:54 +0000

    fix xsd, add contact and host xsd

--- bin/xsd_epp_validation.pl
+++ bin/xsd_epp_validation.pl
@@ -12,19 +12,25 @@ my $xml_doc;
 my $xsd_doc;
 my $is_xml_valid;
 
-# foreach ( qw/epp domain contact host/ ) {
-foreach ( qw/epp/ ) {
+foreach ( qw/epp domain contact host/ ) {
   print "\n##############################\n";
-  print "###Object: " . uc($_) . "\n";
+  print "### Object: " . uc($_) . "\n";
   print "##############################\n";
   $xsd_doc = XML::LibXML::Schema->new(location => './xml/schemas/'. $_. '.xsd');
-  if ($_ eq 'epp') {
+  if ( $_ && $_ eq 'epp' ) {
     foreach (qw/
       hello greeting login login_response logout logout_response
-      poll_req poll_req_response poll_ack poll_ack_response
+      poll_req poll_ack poll_ack_response
       poll_ack_response2 poll_ack_response3/
     ) {
       $xml_doc = XML::LibXML->load_xml(location => './xml/epp/'. $_ . '.xml');
+      try_catch();
+    }
+  } elsif ( $_ && $_ eq 'domain' ) {
+    foreach (qw/
+      domain_check/
+    ) {
+      $xml_doc = XML::LibXML->load_xml(location => './xml/domain/'. $_ . '.xml');
       try_catch();
     }
   }
