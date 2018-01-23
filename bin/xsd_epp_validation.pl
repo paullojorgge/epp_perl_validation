7d8688c71b03020d7a2cfede5b4b7674b9e600a8
Paulo Jorge <paullojorgge@gmail.com>
2018-01-23 15:29:44 +0000

    add missing domain xml samples

--- bin/xsd_epp_validation.pl
+++ bin/xsd_epp_validation.pl
@@ -16,6 +16,7 @@ foreach ( qw/epp domain contact host/ ) {
   print "\n##############################\n";
   print "### Object: " . uc($_) . "\n";
   print "##############################\n";
+  # load the schemas for each object. they are located under: xml/schemas
   $xsd_doc = XML::LibXML::Schema->new(location => './xml/schemas/'. $_. '.xsd');
   if ( $_ && $_ eq 'epp' ) {
     foreach (qw/
@@ -23,13 +24,22 @@ foreach ( qw/epp domain contact host/ ) {
       poll_req poll_ack poll_ack_response
       poll_ack_response2 poll_ack_response3/
     ) {
+      # load the epp xml command / response for every epp action and check if all good
       $xml_doc = XML::LibXML->load_xml(location => './xml/epp/'. $_ . '.xml');
       try_catch();
     }
   } elsif ( $_ && $_ eq 'domain' ) {
     foreach (qw/
-      domain_check/
+      domain_check domain_check_response
+      domain_info domain_info_with_auth domain_info_response domain_info_response2
+      domain_transfer_query domain_transfer_query_response
+      domain_create domain_create_response
+      domain_delete domain_delete_response
+      domain_renew domain_renew_response
+      domain_transfer_request domain_transfer_request_response
+      domain_update domain_update_response/
     ) {
+      # load the epp xml command / response for every domain action and check if all good
       $xml_doc = XML::LibXML->load_xml(location => './xml/domain/'. $_ . '.xml');
       try_catch();
     }
